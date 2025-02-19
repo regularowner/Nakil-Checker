402f417d88f6c549f8277a1c558fa79addce5452
Regular <112163125+regularowner@users.noreply.github.com>
2025-02-19 23:05:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Varsayılan ayarlar
 SETTINGS = {
     "Türü": "Ortaöğretim Genel Müdürlüğü Anadolu Liseleri Okul Listesi",
-    "İL": "MANİSA",
-    "İlce": "SALİHLİ",
-    "okul": "Salihli Türkbirliği Anadolu Lisesi (973804) / SINAVSIZ",
+    "İL": "",
+    "İlce": "",
+    "okul": "",
     "sınıf": 9,
     "kontrol_süresi": 3600,  # saniye cinsinden (1 saat)
     "tema_modu": "dark"  # dark veya light
